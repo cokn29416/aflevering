4fff3d3557312fa43da6e3777e381f31ebe0a12e
cokn29416 <76116013+cokn29416@users.noreply.github.com>
2022-09-15 11:31:23 +0200

    ændringer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-
+//Slide//
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 6){
+    counter = 1;
+  }
+}, 3000);
